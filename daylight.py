6d953e4437f223b4796413818ae7e04c9b74e523
hwetsman <howard@tocdr.com>
2023-10-07 16:33:06 -0500

    Refactor: remove daily print

--- daylight.py
+++ daylight.py
@@ -8,7 +8,7 @@ import pandas as pd
 ts = load.timescale()
 eph = load('de421.bsp')  # Load the ephemeris file
 earth = eph['earth']  # Extract earth
-
+a = st.empty()
 # Geographic location: New Orleans, LA
 new_orleans = Topos(latitude_degrees=29.9511, longitude_degrees=-90.0715)
 
@@ -18,7 +18,6 @@ df = pd.DataFrame()
 # Iterate through each day of the year
 for month in range(1, 13):
     for day in range(1, days_in_month[month] + 1):  # Use the number of days for each month
-
         t0 = ts.utc(2023, month, day, -4, 0, 0)  # Start four hours before midnight of the current day
         t1 = ts.utc(2023, month, day, 28, 0, 0)  # End four hours after midnight of the next day
 
@@ -29,14 +28,21 @@ for month in range(1, 13):
         if len(t) >= 2:  # Both sunrise and sunset should occur
             sunrise_time = t[0] if y[0] else t[1]
             sunset_time = t[1] if y[0] else t[0]
-            daylight_duration = (sunset_time.utc_datetime() - sunrise_time.utc_datetime()).seconds
-            daylight_duration = timedelta(seconds=daylight_duration)
-            # st.write(f"{month:02d}-{day:02d}: {daylight_duration}")
-            df[f"{month:02d}-{day:02d}",'Daylight']= daylight_duration
+            daylight_duration_seconds = (sunset_time.utc_datetime() - sunrise_time.utc_datetime()).seconds
+            daylight_duration_minutes = int(daylight_duration_seconds/60)
+            # daylight_duration = timedelta(seconds=daylight_duration)
+            # st.write(daylight_duration_minutes)
+            a.write(f"Calculating daylight for: {month:02d}-{day:02d}",end = '\r')
+            df.loc[f"{month:02d}-{day:02d}",'Daylight_minutes']= daylight_duration_minutes
+            # st.write(df.loc[f"{month:02d}-{day:02d}",'Daylight'])
         elif len(t) == 1:  # Only one event occurs
             # st.write(f"{month:02d}-{day:02d}: Only one event (either sunrise or sunset)")
-            df[f"{month:02d}-{day:02d}",'Daylight']= None
+            df[f"{month:02d}-{day:02d}",'Daylight_minutes']= None
         else:  # No sunrise or sunset
             # st.write(f"{month:02d}-{day:02d}: No sunrise or sunset")
-            df[f"{month:02d}-{day:02d}",'Daylight']= None
+            df[f"{month:02d}-{day:02d}",'Daylight_minutes']= None
+a.write(df)
+max_daylight =df.Daylight_minutes.max()
+df['Delta_from_max']=max_daylight - df.Daylight_minutes
+st.write(max_daylight)
 st.write(df)
