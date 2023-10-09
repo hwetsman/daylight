37fe9e92cdcf9e895277d74a4e877f0d3fda9be9
hwetsman <howard@tocdr.com>
2023-10-09 16:05:59 -0500

    Refactor: add latitude selector

--- daylight.py
+++ daylight.py
@@ -4,18 +4,24 @@ from datetime import timedelta
 import streamlit as st
 import pandas as pd
 
+
+col1,col2=st.columns([1,4])
+a = col2.empty()
+
 # Load astronomical data
 ts = load.timescale()
 eph = load('de421.bsp')  # Load the ephemeris file
 earth = eph['earth']  # Extract earth
 
-# Geographic location: New Orleans, LA
-new_orleans = Topos(latitude_degrees=29.9511, longitude_degrees=-90.0715)
 
-col1,col2=st.columns([1,4])
-a = col2.empty()
-max_light_box = col1.selectbox('Choose minutes for max lightbox',[5,10,15,20,25,30,35,40])
-light_box_interval = col1.selectbox('Choose an interval for light box time increase',[1,2,3,4,5])
+lat = col1.slider('Lattitude to one decimel place', max_value=60.0, min_value=20.0, value=30.0, step=0.1)
+location= Topos(latitude_degrees=lat, longitude_degrees=-90)
+# Geographic location: New Orleans, LA
+# new_orleans = Topos(latitude_degrees=29.9511, longitude_degrees=-90.0715)
+
+
+max_light_box = col1.selectbox('Choose minutes for max lightbox',[5,10,15,20,25,30,35,40,45,50],index=5)
+light_box_interval = col1.selectbox('Choose an interval for light box time increase',[1,2,3,4,5],index=4)
 # Days in each month for the year 2023 (which is not a leap year)
 days_in_month = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
 df = pd.DataFrame()
@@ -25,7 +31,8 @@ for month in range(1, 13):
         t0 = ts.utc(2023, month, day, -4, 0, 0)  # Start four hours before midnight of the current day
         t1 = ts.utc(2023, month, day, 28, 0, 0)  # End four hours after midnight of the next day
 
-        t, y = almanac.find_discrete(t0, t1, almanac.sunrise_sunset(eph, new_orleans))
+        # t, y = almanac.find_discrete(t0, t1, almanac.sunrise_sunset(eph, new_orleans))
+        t, y = almanac.find_discrete(t0, t1, almanac.sunrise_sunset(eph, location))
 
         # print(t)  # Debugging line to print the times array
 
@@ -53,6 +60,7 @@ df = df.sort_values('Date')
 max_daylight =df.Daylight_minutes.max()
 min_daylight = df.Daylight_minutes.min()
 annual_difference = max_daylight - min_daylight
+col2.write(f'The difference between the maximum and minimum of daylight at your latitude is {int(annual_difference)} minutes.')
 difference_to_maxbox_ratio = int(annual_difference/max_light_box)
 minutes_to_interval_change = difference_to_maxbox_ratio * light_box_interval
 df['Delta_from_max']=max_daylight - df.Daylight_minutes
